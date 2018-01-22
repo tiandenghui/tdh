ce93d013bbaeadd92538c86c99cbe94c81d920d0
董文强 <826112946@qq.com>
2018-01-22 17:25:44 +0800

    Signed-off-by: 董文强 <826112946@qq.com> AAA

--- horcrux-spring-cloud-zdy/config-server/src/main/java/com/kuliao/horcrux/app/ConfigServerApplication.java
+++ horcrux-spring-cloud-zdy/config-server/src/main/java/com/kuliao/horcrux/app/ConfigServerApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
